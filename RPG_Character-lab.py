5278635462dc747790ea0325203332bdf68ce954
Kamil Banasiak <kamilbanasiak@vp.pl>
2026-07-06 16:12:49 +0200

    Add RPG_Character-lab

---
+++ RPG_Character-lab.py
@@ -0,0 +1,25 @@
+full_dot = '●'
+empty_dot = '○'
+
+def create_character(character_name, strength, intelligence, charisma):
+    if type(character_name) != str:
+        return 'The character name should be a string'
+    if character_name == '':
+        return 'The character should have a name'
+    if len(character_name) > 10:
+        return 'The character name is too long'
+    if ' ' in character_name:
+        return 'The character name should not contain spaces'
+    if not (type(strength) == int and type(intelligence) == int and type(charisma) == int):
+        return 'All stats should be integers'
+    if strength < 1 or intelligence < 1 or charisma < 1:
+        return 'All stats should be no less than 1'
+    if strength > 4 or intelligence > 4 or charisma > 4:
+        return 'All stats should be no more than 4'
+    if strength + intelligence + charisma != 7:
+        return 'The character should start with 7 points'
+    line1 = character_name
+    line2 = 'STR ' + strength * full_dot + (10 - strength) * empty_dot
+    line3 = 'INT ' + intelligence * full_dot + (10 - intelligence) * empty_dot
+    line4 = 'CHA ' + charisma * full_dot + (10 - charisma) * empty_dot
+    return f'{line1}\n{line2}\n{line3}\n{line4}'
